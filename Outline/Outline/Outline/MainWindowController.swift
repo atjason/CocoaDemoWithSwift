c467d0b36a58994dca5f376d668777d8484a72fd
Jason Zheng <quietjason@gmail.com>
2016-04-06 21:52:28 +0800

    Implement NSOutlineViewDataSource.

--- Outline/Outline/Outline/MainWindowController.swift
+++ Outline/Outline/Outline/MainWindowController.swift
@@ -8,11 +8,91 @@
 
 import Cocoa
 
-class MainWindowController: NSWindowController {
+class MainWindowController: NSWindowController, NSOutlineViewDataSource {
   
   @IBOutlet weak var outlineView: NSOutlineView!
   
+  var nodes = [Node]()
+  
   override var windowNibName: String? {
     return "MainWindowController"
+  }
+  
+  func initNodes() {
+    let node = Node(title: "Node")
+    let group = Node(title: "Group")
+    let nodeA = Node(title: "Node A")
+    let nodeB = Node(title: "Node B")
+    
+    group.isGroup = true
+    group.children = [nodeA, nodeB]
+    
+    nodes = [node, group]
+  }
+  
+  override func windowWillLoad() {
+    initNodes()
+  }
+  
+  override func windowDidLoad() {
+    super.windowDidLoad()    
+    
+    let index = 0
+    outlineView.scrollRowToVisible(index)
+    outlineView.expandItem(nil, expandChildren: true)
+  }
+  
+  // MARK: - NSOutlineViewDataSource
+  
+  func outlineView(outlineView: NSOutlineView,
+                   child index: Int, ofItem item: AnyObject?) -> AnyObject {
+    
+    if item == nil { // Case of virtual root
+      return nodes[index]
+      
+    } else if let hostsItem = item as? Node {
+      return hostsItem.children[index]
+      
+    } else {
+      // TODO: Use better object
+      print("Error: invalid object.")
+      return ""
+    }
+  }
+  
+  func outlineView(outlineView: NSOutlineView,
+                   isItemExpandable item: AnyObject) -> Bool {
+    guard let hostsItem = item as? Node else {
+      print("Error: invalid object.")
+      return false
+    }
+    
+    return hostsItem.children.count > 0
+  }
+  
+  func outlineView(outlineView: NSOutlineView,
+                   numberOfChildrenOfItem item: AnyObject?) -> Int {
+    
+    if item == nil { // Case of virtual root
+      return nodes.count
+      
+    } else if let hostsItem = item as? Node {
+      return hostsItem.children.count
+      
+    } else {
+      print("Error: invalid object.")
+      return 0
+    }
+  }
+  
+  func outlineView(outlineView: NSOutlineView,
+                   objectValueForTableColumn tableColumn: NSTableColumn?,
+                                             byItem item: AnyObject?) -> AnyObject? {
+    guard let hostsItem = item as? Node else {
+      print("Error: invalid object.")
+      return nil
+    }
+    
+    return hostsItem
   }
 }
