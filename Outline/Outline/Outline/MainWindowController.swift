3f7ffaa8efb62b5e08d2f53da693da05055a80cf
Jason Zheng <quietjason@gmail.com>
2016-04-06 22:34:00 +0800

    Implement other UI Operations.

--- Outline/Outline/Outline/MainWindowController.swift
+++ Outline/Outline/Outline/MainWindowController.swift
@@ -8,10 +8,21 @@
 
 import Cocoa
 
+enum MoveDirection {
+  case Up, Down
+}
+
 class MainWindowController: NSWindowController,
     NSOutlineViewDataSource, NSOutlineViewDelegate {
   
   @IBOutlet weak var outlineView: NSOutlineView!
+  
+  dynamic var removeButtonEnabled = false
+  dynamic var actionButtonEnabled = false
+  dynamic var editMenuItemEnabled = false
+  dynamic var moveUpMenuItemEnabled = false
+  dynamic var moveDownMenuItemEnabled = false
+  dynamic var hostsTextFiledEnabled = false
   
   var nodes = [Node]()
   
@@ -45,6 +56,8 @@ class MainWindowController: NSWindowController,
     let index = 0
     outlineView.scrollRowToVisible(index)
     outlineView.expandItem(nil, expandChildren: true)
+    
+    updateUIStatus()
   }
   
   // MARK: - Helper Functions
@@ -94,6 +107,80 @@ class MainWindowController: NSWindowController,
     let row = outlineView.selectedRow
     if row != -1 {
       outlineView.editColumn(0, row: row, withEvent: nil, select: true)
+    }
+  }
+  
+  func removeItem(item: Node, inout itemArray: [Node]) {
+    if let index = itemArray.indexOf(item) {
+      itemArray.removeAtIndex(index)
+    }
+  }
+  
+  func moveItem(item: Node, inout itemArray: [Node], direction: MoveDirection) {
+    if let index = itemArray.indexOf(item) {
+      var toIndex = index
+      switch direction {
+      case .Up:
+        toIndex -= 1
+      default:
+        toIndex += 1
+      }
+      
+      if toIndex >= 0 && toIndex < itemArray.count {
+        swap(&itemArray[index], &itemArray[toIndex])
+      }
+    }
+  }
+  
+  func moveItemInOutlineView(direction: MoveDirection) {
+    if let selectedItem = outlineView.itemAtRow(outlineView.selectedRow) as? Node {
+      
+      if let parent = selectedItem.parent {
+        moveItem(selectedItem, itemArray: &parent.children, direction: direction)
+        
+      } else {
+        moveItem(selectedItem, itemArray: &nodes, direction: direction)
+      }
+      
+      outlineView.reloadData()
+      selectItem(selectedItem, outlineView: outlineView)
+    }
+  }
+  
+  func canMoveItemInOutlineView(direction: MoveDirection) -> Bool {
+    
+    if let selectedItem = outlineView.itemAtRow(outlineView.selectedRow) as? Node {
+      
+      let index = outlineView.childIndexForItem(selectedItem)
+      let count = outlineView.numberOfChildrenOfItem(selectedItem.parent)
+      
+      switch direction {
+      case .Up:
+        return index > 0
+      default:
+        return index < (count - 1)
+      }
+    }
+    
+    return false
+  }
+  
+  func updateUIStatus() {
+    if outlineView.selectedRow == -1 {
+      removeButtonEnabled = false
+      
+      actionButtonEnabled = false
+      editMenuItemEnabled = false
+      moveUpMenuItemEnabled = false
+      moveDownMenuItemEnabled = false
+      
+    } else {
+      removeButtonEnabled = true
+      
+      actionButtonEnabled = true
+      editMenuItemEnabled = true
+      moveUpMenuItemEnabled = canMoveItemInOutlineView(.Up)
+      moveDownMenuItemEnabled = canMoveItemInOutlineView(.Down)
     }
   }
   
@@ -220,6 +307,65 @@ class MainWindowController: NSWindowController,
     return node
   }
   
+  @IBAction func removeItem(sender: NSButton) {
+    
+    let selectedRow = outlineView.selectedRow
+    
+    if let selectedItem = outlineView.itemAtRow(outlineView.selectedRow) as? Node {
+      
+      // For root item
+      if selectedItem.parent == nil && !selectedItem.isGroup {
+        
+        removeItem(selectedItem, itemArray: &nodes)
+        
+      } else if selectedItem.isGroup { // For group
+        selectedItem.children.removeAll()
+        
+        removeItem(selectedItem, itemArray: &nodes)
+        
+      } else { // For child item
+        
+        if let parent = selectedItem.parent {
+          
+          removeItem(selectedItem, itemArray: &parent.children)
+          
+          if parent.children.isEmpty {
+            removeItem(parent, itemArray: &nodes)
+          }
+        }
+      }
+      
+      outlineView.reloadData()
+      
+      var newSelectedRow = selectedRow
+      
+      while newSelectedRow >= outlineView.numberOfRows {
+        newSelectedRow -= 1
+      }
+      
+      outlineView.scrollRowToVisible(newSelectedRow)
+      outlineView.selectRowIndexes(NSIndexSet(index: newSelectedRow), byExtendingSelection: false)
+      
+      if newSelectedRow == -1 {
+        // Note: when remove last item, outlineViewSelectionDidChange will not be called.
+        //       Thus maunally update status here.
+        updateUIStatus()
+      }
+    }
+  }
+  
+  @IBAction func editSelectedItem(sender: NSObject) {
+    editSelectedItemInOutlineView(outlineView)
+  }
+  
+  @IBAction func moveItemUp(sender: NSObject) {
+    moveItemInOutlineView(.Up)
+  }
+  
+  @IBAction func moveItemDown(sender: NSObject) {
+    moveItemInOutlineView(.Down)
+  }
+  
   // MARK: - NSOutlineViewDelegate
   
   func outlineView(outlineView: NSOutlineView, isGroupItem item: AnyObject) -> Bool {
@@ -237,5 +383,9 @@ class MainWindowController: NSWindowController,
     // In this case use get the column by outlineView.tableColumns[0]
     let identifier = tableColumn?.identifier ?? outlineView.tableColumns[0].identifier
     return outlineView.makeViewWithIdentifier(identifier, owner: self)
+  }
+  
+  func outlineViewSelectionDidChange(notification: NSNotification) {
+    updateUIStatus()
   }
 }
