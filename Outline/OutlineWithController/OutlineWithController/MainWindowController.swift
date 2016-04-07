f9148114ec25db9a10e654f776396d1b2a2952a0
Jason Zheng <quietjason@gmail.com>
2016-04-07 11:18:08 +0800

    Prepare Demo Nodes Data.

--- Outline/OutlineWithController/OutlineWithController/MainWindowController.swift
+++ Outline/OutlineWithController/OutlineWithController/MainWindowController.swift
@@ -9,7 +9,27 @@
 import Cocoa
 
 class MainWindowController: NSWindowController {
+  
+  dynamic var nodes = [Node]()
+  
   override var windowNibName: String? {
     return "MainWindowController"
+  }
+  
+  func initNodes() {
+    let node = Node(title: "Node")
+    let group = Node(title: "Group")
+    let nodeA = Node(title: "Node A")
+    let nodeB = Node(title: "Node B")
+    
+    group.children = [nodeA, nodeB]
+    
+    nodes = [node, group]
+  }
+  
+  // MARK: - lifecycle
+  
+  override func windowWillLoad() {
+    initNodes()
   }
 }
