5d3886d3e7a0c45ff753be2c543851f639a334ef
Jason Zheng <quietjason@gmail.com>
2016-04-08 22:00:01 +0800

    Add pop over and controller.

---
+++ StatusBarApp/StatusBarApp/StatusBarApp/PopoverViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PopoverViewController.swift
+//  StatusBarApp
+//
+//  Created by Jason Zheng on 4/8/16.
+//  Copyright © 2016 Jason Zheng. All rights reserved.
+//
+
+import Cocoa
+
+class PopoverViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
