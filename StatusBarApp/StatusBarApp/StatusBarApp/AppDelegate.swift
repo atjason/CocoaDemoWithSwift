8c3e723da37b0d11c07c58466feb59afdf8dd52a
Jason Zheng <quietjason@gmail.com>
2016-04-08 11:13:18 +0800

    Update Outline/Outline/Outline/MainWindowController.xib

---
+++ StatusBarApp/StatusBarApp/StatusBarApp/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  StatusBarApp
+//
+//  Created by Jason Zheng on 4/8/16.
+//  Copyright © 2016 Jason Zheng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ StatusBarApp/StatusBarApp/StatusBarApp/StatusItemController.swift
@@ -0,0 +1,18 @@
+//
+//  StatusItemController.swift
+//  StatusBarApp
+//
+//  Created by Jason Zheng on 4/8/16.
+//  Copyright © 2016 Jason Zheng. All rights reserved.
+//
+
+import Cocoa
+
+class StatusItemController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
