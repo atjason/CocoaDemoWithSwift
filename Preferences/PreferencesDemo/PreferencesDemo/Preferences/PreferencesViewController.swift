bda6615b12094f7cb2d225aae4f35c219dca3381
Jason Zheng <quietjason@gmail.com>
2016-04-13 11:10:58 +0800

    Set size for each tab in preferences.

--- Preferences/PreferencesDemo/PreferencesDemo/Preferences/PreferencesViewController.swift
+++ Preferences/PreferencesDemo/PreferencesDemo/Preferences/PreferencesViewController.swift
@@ -8,11 +8,45 @@
 
 import Cocoa
 
-class PreferencesViewController: NSTabViewController {
+/*!
+ * In the storyboard, ensure
+     the view controller's transition checkboxes are all off
+     the NSTabView's delegate is set to this controller object
+ */
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do view setup here.
-    }
+class PreferencesViewController: NSTabViewController {
+  
+  lazy var originalSizes = [String : NSSize]()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  // MARK: - NSTabViewDelegate
+  
+  override func tabView(tabView: NSTabView, willSelectTabViewItem tabViewItem: NSTabViewItem?) {
+    super.tabView(tabView, willSelectTabViewItem: tabViewItem)
     
+    _ = tabView.selectedTabViewItem
+    let originalSize = self.originalSizes[tabViewItem!.label]
+    if (originalSize == nil) {
+      self.originalSizes[tabViewItem!.label] = (tabViewItem!.view?.frame.size)!
+    }
+  }
+  
+  override func tabView(tabView: NSTabView, didSelectTabViewItem tabViewItem: NSTabViewItem?) {
+    super.tabView(tabView, didSelectTabViewItem: tabViewItem)
+    
+    let window = self.view.window
+    if (window != nil) {
+      window?.title = tabViewItem!.label
+      let size = (self.originalSizes[tabViewItem!.label])!
+      let contentFrame = (window?.frameRectForContentRect(NSMakeRect(0.0, 0.0, size.width, size.height)))!
+      var frame = (window?.frame)!
+      frame.origin.y = frame.origin.y + (frame.size.height - contentFrame.size.height)
+      frame.size.height = contentFrame.size.height;
+      frame.size.width = contentFrame.size.width;
+      window?.setFrame(frame, display: false, animate: true)
+    }
+  }
 }
