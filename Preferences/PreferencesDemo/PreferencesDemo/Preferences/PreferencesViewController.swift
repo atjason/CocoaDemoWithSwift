198478df8b67db72a8e95115e4d37a71b0b6d7fb
Jason Zheng <quietjason@gmail.com>
2016-04-13 12:43:57 +0800

    Use SizeArchiver to save tab view's size.

--- Preferences/PreferencesDemo/PreferencesDemo/Preferences/PreferencesViewController.swift
+++ Preferences/PreferencesDemo/PreferencesDemo/Preferences/PreferencesViewController.swift
@@ -16,14 +16,46 @@ import Cocoa
 
 class PreferencesViewController: NSTabViewController {
   
-  lazy var tabViewSizes = [String : NSSize]()
+  private let tabViewSizesKey = "Preferences Tab View Sizes"
+  
+  var tabViewSizes = [String: SizeArchiver]()
   
   override func viewDidLoad() {
-    super.viewDidLoad()
+    readTabViewSizes()
     
-    for tabViewItem in tabViewItems {
-      tabViewSizes[tabViewItem.label] = tabViewItem.view?.frame.size
+    super.viewDidLoad()
+  }
+  
+  override func viewWillDisappear() {
+    saveTabViewSizes()
+  }
+  
+  // MARK: - Helper
+  
+  func readTabViewSizes() {
+    var sizesInitialized = false
+    
+    let userDefaults = NSUserDefaults.standardUserDefaults()
+    if let data = userDefaults.objectForKey(tabViewSizesKey) as? NSData {
+      if let sizes = NSKeyedUnarchiver.unarchiveObjectWithData(data) as? [String: SizeArchiver] {
+        tabViewSizes = sizes
+        sizesInitialized = true
+      }
     }
+    
+    if !sizesInitialized {
+      for tabViewItem in tabViewItems {
+        if let size = tabViewItem.view?.frame.size {
+          tabViewSizes[tabViewItem.label] = SizeArchiver(size: size)
+        }
+      }
+    }
+  }
+  
+  func saveTabViewSizes() {
+    let data = NSKeyedArchiver.archivedDataWithRootObject(tabViewSizes)
+    let userDefaults = NSUserDefaults.standardUserDefaults()
+    userDefaults.setObject(data, forKey: tabViewSizesKey)
   }
   
   // MARK: - NSTabViewDelegate
@@ -31,25 +63,35 @@ class PreferencesViewController: NSTabViewController {
   override func tabView(tabView: NSTabView, willSelectTabViewItem tabViewItem: NSTabViewItem?) {
     super.tabView(tabView, willSelectTabViewItem: tabViewItem)
     
+    // Save the size of the tab view item to be unselected
     if let selectedTabView = tabView.selectedTabViewItem {
-      tabViewSizes[selectedTabView.label] = selectedTabView.view?.frame.size
+      tabViewSizes[selectedTabView.label] = SizeArchiver(size: view.frame.size)
+    }
+    
+    // Set the size of the tab view item to be selected
+    if let tabViewItem = tabViewItem {
+      if let size = tabViewSizes[tabViewItem.label]?.size {
+        view.setFrameSize(size)
+      }
     }
   }
   
   override func tabView(tabView: NSTabView, didSelectTabViewItem tabViewItem: NSTabViewItem?) {
     super.tabView(tabView, didSelectTabViewItem: tabViewItem)
     
-    if let window = view.window  {
-      window.title = tabViewItem!.label
-      
-      if let size = tabViewSizes[tabViewItem!.label] {
-        let contentFrame = window.frameRectForContentRect(NSMakeRect(0.0, 0.0, size.width, size.height))
+    if let tabViewItem = tabView.selectedTabViewItem {
+      if let window = view.window  {
+        window.title = tabViewItem.label
         
-        var frame = window.frame
-        frame.origin.y = frame.origin.y + (frame.size.height - contentFrame.size.height)
-        frame.size.height = contentFrame.size.height;
-        frame.size.width = contentFrame.size.width;
-        window.setFrame(frame, display: false, animate: true)
+        if let size = tabViewSizes[tabViewItem.label] {
+          let contentFrame = window.frameRectForContentRect(NSMakeRect(0.0, 0.0, size.width, size.height))
+          
+          var frame = window.frame
+          frame.origin.y = frame.origin.y + (frame.size.height - contentFrame.size.height)
+          frame.size.height = contentFrame.size.height;
+          frame.size.width = contentFrame.size.width;
+          window.setFrame(frame, display: false, animate: true)
+        }
       }
     }
   }
