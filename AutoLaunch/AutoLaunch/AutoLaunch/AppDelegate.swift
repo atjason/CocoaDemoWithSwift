429df1a9e38c52ef604ec5f461ad0d1b07d1776d
Jason Zheng <quietjason@gmail.com>
2016-04-14 16:53:53 +0800

    Init AutoLaunch project.

---
+++ AutoLaunch/AutoLaunch/AutoLaunch/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  AutoLaunch
+//
+//  Created by Jason Zheng on 4/14/16.
+//  Copyright © 2016 Jason Zheng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  var mainWindowController: MainWindowController?
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+      
+    let mainWindowController = MainWindowController()
+    mainWindowController.showWindow(self)
+    
+    self.mainWindowController = mainWindowController
+  }
+}
+
---
+++ AutoLaunch/AutoLaunch/AutoLaunch/MainWindowController.swift
@@ -0,0 +1,15 @@
+//
+//  MainWindowController.swift
+//  AutoLaunch
+//
+//  Created by Jason Zheng on 4/14/16.
+//  Copyright © 2016 Jason Zheng. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+  override var windowNibName: String? {
+    return "MainWindowController"
+  }
+}
