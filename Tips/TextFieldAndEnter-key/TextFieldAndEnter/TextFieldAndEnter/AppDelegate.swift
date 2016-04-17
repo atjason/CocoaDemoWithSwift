a2f4beedb5976ddea7ef9045b025f1a50349f682
Jason Zheng <quietjason@gmail.com>
2016-04-17 11:54:25 +0800

    Add Tips/TextFieldAndEnter-key.

---
+++ Tips/TextFieldAndEnter-key/TextFieldAndEnter/TextFieldAndEnter/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  TextFieldAndEnter
+//
+//  Created by Jason Zheng on 4/17/16.
+//  Copyright © 2016 Jason Zheng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  var mainWindowController: MainWindowController?
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+      
+    let mainWindowController = MainWindowController()
+    mainWindowController.showWindow(self)
+    
+    self.mainWindowController = mainWindowController
+  }
+}
+
---
+++ Tips/TextFieldAndEnter-key/TextFieldAndEnter/TextFieldAndEnter/MainWindowController.swift
@@ -0,0 +1,15 @@
+//
+//  MainWindowController.swift
+//  TextFieldAndEnter
+//
+//  Created by Jason Zheng on 4/17/16.
+//  Copyright © 2016 Jason Zheng. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+  override var windowNibName: String? {
+    return "MainWindowController"
+  }
+}
