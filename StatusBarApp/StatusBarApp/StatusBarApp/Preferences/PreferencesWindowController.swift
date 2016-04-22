9f1ad25d2481bec8c66a2cc776356e5f79955042
Jason Zheng <quietjason@gmail.com>
2016-04-22 15:25:41 +0800

    Add class PreferencesWindowController.

---
+++ StatusBarApp/StatusBarApp/StatusBarApp/Preferences/PreferencesWindowController.swift
@@ -0,0 +1,20 @@
+//
+//  PreferencesWindowController.swift
+//  StatusBarApp
+//
+//  Created by Jason Zheng on 4/22/16.
+//  Copyright © 2016 Jason Zheng. All rights reserved.
+//
+
+import Cocoa
+
+class PreferencesWindowController: NSWindowController {
+  
+  override var windowNibName: String? {
+    return "PreferencesWindowController"
+  }
+  
+  override func windowDidLoad() {
+    super.windowDidLoad()
+  }  
+}
