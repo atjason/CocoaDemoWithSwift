5d42cff88fc1498d34c1d223f1f57b726f55da51
Jason <quietjason@gmail.com>
2016-10-04 21:43:27 +0800

    Add Encode.playground.

---
+++ Tips/Encode/Encode.playground/Contents.swift
@@ -0,0 +1,30 @@
+
+import Cocoa
+import Foundation
+
+extension String {
+  func encode(_ characterSet: CharacterSet) -> String {
+    return self.addingPercentEncoding(withAllowedCharacters: characterSet) ?? self
+  }
+}
+
+let complexString = "\"#%<>/\\:;?@[]^`{|}"
+print("Origional:         \t" + complexString)
+
+// urlHostAllowed      "#%/<>?@\^`{|}
+print("urlHostAllowed:    \t" + complexString.encode(.urlHostAllowed))
+
+// urlHostAllowed      "#%;<>?[\]^`{|}
+print("urlPathAllowed:    \t" + complexString.encode(.urlPathAllowed))
+
+// urlQueryAllowed     "#%<>[\]^`{|}
+print("urlQueryAllowed:   \t" + complexString.encode(.urlQueryAllowed))
+
+// urlUserAllowed      "#%/:<>?@[\]^`
+print("urlUserAllowed:    \t" + complexString.encode(.urlUserAllowed))
+
+// urlPasswordAllowed  "#%/:<>?@[\]^`{|}
+print("urlPasswordAllowed:\t" + complexString.encode(.urlPasswordAllowed))
+
+// urlFragmentAllowed  "#%<>[\]^`{|}
+print("urlFragmentAllowed:\t" + complexString.encode(.urlFragmentAllowed))
